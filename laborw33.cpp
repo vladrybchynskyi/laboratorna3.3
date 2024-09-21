e9424d65017fd3c85667e7a08de8d8096071311f
vladrybchynskyi <vladyslav.rybchynskyi.ri.2024@lpnu.ua>
2024-09-22 00:05:53 +0300

    Update laborw33.cpp

--- laborw33.cpp
+++ laborw33.cpp
@@ -5,7 +5,7 @@ int main() {
     double x;
     double R;
     double y;
-    
+
 
     cout << "R ="; cin >> R;
     cout << "x ="; cin >> x;
@@ -13,19 +13,21 @@ int main() {
     if (x <= -6)
         y = R;
     else
-        if (x >= -6 && R >= x)
-           y = (5 * R - R * R) / (6 - R);
-          else
-              if ( 0 <= x && x <= R)
-                y = R - x;
-             else
-                if (R <= x && x < (R + 1))
-                    y = sqrt(R * R - x * x);
+        if (x >= -6 && -R >= x)
+            y = (5 * R - R * R) / (6 - R);
+        else
+            if (x >= -R && 0 >= x)
+                y = -sqrt(-x * x - 2 * x * R) + R;
+            else
+                if (0 <= x && x <= R)
+                    y = R - x;
                 else
-                    x = R - x;
+                    if (R <= x && x < ++R)
+                        y = sqrt(R * R - x * x);
+                    else
+                        y = R - x;
     cout << endl;
     cout << "y =" << y << endl;
 
     cin.get();
     return 0;
-}
